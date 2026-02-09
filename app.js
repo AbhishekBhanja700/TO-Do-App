84203cfdd62b9db66ba2154a38f8f6d881db30aa
Abhishek Bhanja <abhishekbhanjawork@gmail.com>
2026-02-09 14:12:12 +0530

    Create app.js
    added JS file

---
+++ app.js
@@ -0,0 +1,31 @@
+let todo = [];
+
+let req = prompt("please enter your request");
+
+while(true){
+
+    if(req == "quit"){
+        console.log("Quitting app");
+        break;
+    
+    }else if(req == "list"){
+        console.log("------------------")
+        for(let i =0 ; i<todo.length; i++){
+            console.log(i, todo[i]);
+        }
+        console.log("------------------")
+
+    }else if(req == "add"){
+        let task = prompt("Please enter the task you want to add");
+        todo.push(task);
+        console.log("task added");
+    }else if(req == "delete"){
+        let idx = prompt("Please enter the tast index");
+        todo.splice(idx,1);
+        console.log("Task deleted");
+    }else{
+        console.log("Invalid request");
+    }
+
+    req = prompt("please enter your request");
+}
